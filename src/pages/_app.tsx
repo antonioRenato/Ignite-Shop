eb4bc2c7e8f844566a1f8e8ae7b15d9ded4d1545
antonioRenato <antoniorpfs@gmail.com>
2023-04-04 20:38:51 -0300

    start css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
+import { globalStyle } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  globalStyle();
+
   return <Component {...pageProps} />
 }
