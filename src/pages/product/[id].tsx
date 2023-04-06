bbbdf5539f572b5b607240d55ecb45190da4ce6f
antonioRenato <antoniorpfs@gmail.com>
2023-04-06 05:15:39 -0300

    created html and css to page product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
 
   return(
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo odit quas quae eaque est, aspernatur ab error, dolorum doloremque veritatis amet soluta repellat iste dolores accusantium sed nihil laborum?</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
