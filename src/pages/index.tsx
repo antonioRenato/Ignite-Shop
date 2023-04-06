0a1117f734b4959973e1d4238adb8557b1849300
antonioRenato <antoniorpfs@gmail.com>
2023-04-06 04:43:34 -0300

    starting to use ssg with getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,9 @@ import { useKeenSlider } from 'keen-slider/react'
 
 import Image from "next/image";
 
-import camiseta1 from '../assets/1.png'
-import camiseta2 from '../assets/2.png'
-import camiseta3 from '../assets/3.png'
-import camiseta4 from '../assets/4.png'
-
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "@/lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -48,7 +43,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
@@ -60,7 +55,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: (price.unit_amount || 0)  / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount as number / 100),
     }
   })
   
@@ -68,5 +66,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // A cada 2 horas
   };
 };
